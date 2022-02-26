dcb8b31af09f7b664e0bee71cb50e81078e3f2b1
Daniel Mallcott <daniel.mallcott@gmail.com>
2022-02-26 21:57:48 +0000

    Adding migration

--- eth-contracts/migrations/2_deploy_contracts.js
+++ eth-contracts/migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
+var SquareVerifier = artifacts.require("SquareVerifier");
+var SolutionVerifier = artifacts.require("SolutionVerifier");
 
-
-module.exports = function(deployer) {
-    
+module.exports = function (deployer) {
+    deployer.deploy(SquareVerifier).then(function () {
+        return deployer.deploy(SolutionVerifier, SquareVerifier.address);
+    });
 };
